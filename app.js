15d1a7be8f7c6ca7fba3874fbffb4639fe15cce2
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-06-30 19:55:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server=http.createServer(function(req , res){
+    res.write('HAI , Hello this is normal text');
+    res.write("<h1> HTML CODE </h1>");
+    res.end();
+})
+server.listen(4000);
